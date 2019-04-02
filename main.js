03f779852c575ee65eabec8bd9126f865d1827cf
Adam Grider <adam.h.grider@gmail.com>
2019-04-02 13:48:12 -0400

    add notes and offsets

--- main.js
+++ main.js
@@ -1,49 +1,75 @@
-//Heading
+//Defining the function
 function calendar(thisMonth, thisYear = 2019) {
+  //Printing year/month headers
   let days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   week = days.join(" ")
   console.log(months[(thisMonth - 1)] + " " + thisYear);
   console.log(week);
+
+  //Setup days per month (incl. leap years)
   let dates = [];
   let DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   if (thisYear % 4 === 0) {
     DAYS_IN_MONTH[1] = 29;
   }
+  
+  //fill dates array with full month of values
   for (let i = 1; i <= DAYS_IN_MONTH[(thisMonth - 1)]; i++) {
     dates.push(i.toString().padStart(2, " "));
   }
-  const DAYS_IN_WEEK = 7;
-  //Loop for the weekday of the 1st of the month:
-  //Su=0 Mo=1 Tu=2 We=3
+  //Setup variables for annual offset from 2019 as base year
   let yearOffset = 0;
   let diffYears = [];
   let leapYears = [];
+
+  //Offset function
   function offset (mo, year) {
+  //1st day of each month of 2019 (default) - 0:sunday-6:saturday
+    let daysOffset = [2, 5, 5, 1, 3, 6, 1, 4, 0, 2, 5, 0];
+    //Offset for years before or after 2019
     if (year > 2019) {
       yearOffset = (year - 2019);
-      for (let i = 1; i < (year - 2019); i++) {
+      for (let i = 1; i <= (year - 2019); i++) {
         diffYears.push(2019 + i);
-        if (diffYears[i] % 4 === 0) {
+        if (diffYears[i] % 4 === 0 && mo > 2) {
           leapYears.push(diffYears[i]);
         }
-        yearOffset += leapYears.length;
       } 
+      yearOffset += leapYears.length;
     } else if (year < 2019) {
-      for (let i = 1; i < (2019 - year); i++) {
+      yearOffset = (year - 2019);
+      for (let i = 1; i <= (2019 - year); i++) {
         diffYears.push(2019 - i);
         if (diffYears[i] % 4 === 0) {
           leapYears.push(diffYears[i]);
         }
-        yearOffset += leapYears.length;
+      }
+      //adjust yearOffset for number of leap years
+      yearOffset -= leapYears.length;
+    }
+    //adjust daysOffset to account for yearOffset and correct to 0-6 for weekdays
+    for (i = 0; i < 12; i++) {
+      daysOffset[i] += yearOffset;
+      if (daysOffset[i] < 0) {
+        while (daysOffset[i] < 0) {
+          daysOffset[i] += 7;
+        }
+      } else if (daysOffset[i] > 6) {
+        while (daysOffset[i] > 6) {
+          daysOffset[i] -= 7;
+        }
       }
     }
-    let daysOffset = [2, 5, 5, 1, 3, 6, 1, 4, 0, 2, 5, 0]
+    //insert empty strings for offset days at start of each month
     for (let i = 0; i < daysOffset[(mo - 1)]; i++) {
       dates.unshift("  ");
     }
   }
+  //call offset function for thisMonth & thisYear - established by calling calendar()
   offset(thisMonth, thisYear);
+  //Define number of weeks and print 7 array values per line
+  const DAYS_IN_WEEK = 7;
   for (let i = 0; i < (Math.floor((DAYS_IN_MONTH[(thisMonth - 1)]) / DAYS_IN_WEEK) + 2); i++) {
     let weeklyDates = [];
     for (let j = 0; j < 7; j++) {
@@ -56,4 +82,8 @@ function calendar(thisMonth, thisYear = 2019) {
     }
   }
 }
-calendar(4);
+//call function - calendar(m, yyyy)
+/*for (let i = 1; i < 13; i++) {
+  calendar(i);
+}*/
+calendar(3);
